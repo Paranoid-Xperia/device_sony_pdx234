2a2b024c24bff1dd1accbcd15239e03d2c5bd165
Anay Wadhera <anay1018@gmail.com>
2023-10-16 00:06:24 -0400

    pdx234: Patch keymint to include rkp AIDL for missing symbol
    Change-Id: Iad47fb49883eb3e6af3726815e2f996c35aaab09

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,17 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/bin/hw/android.hardware.security.keymint-service-qti)
+            "${PATCHELF}" --add-needed android.hardware.security.rkp-V1-ndk.so "${2}"
+            ;;
+         vendor/lib64/libqtikeymint.so)
+            "${PATCHELF}" --add-needed android.hardware.security.rkp-V1-ndk.so "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
