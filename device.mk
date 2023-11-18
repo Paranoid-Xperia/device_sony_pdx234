11ace64bec8a341086b6b435bdea209cd35eb895
SpectreDev1 <mohammad.galaxy07@gmail.com>
2023-11-18 15:46:40 +0000

    pdx234: Import device specific init scripts

--- device.mk
+++ device.mk
@@ -70,6 +70,13 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom
+    init.target.rc \
+    init.sony-device-common.rc \
+    init.sony-platform.rc \
+    init.sony.rc \
+    init.target.rc \
+    ueventd.rc \
+    ueventd.sony.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
