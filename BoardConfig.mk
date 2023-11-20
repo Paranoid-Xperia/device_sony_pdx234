982feef174dd9eb1d06e3f974fff69997e810295
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2023-11-20 10:00:05 +0000

    pdx234: remove qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,6 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # SELinux
-include device/qcom/sepolicy_vndr/SEPolicy.mk
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
